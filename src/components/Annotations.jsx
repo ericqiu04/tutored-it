cbc4ee006334cd10e09a269e13f6190cb1f8f2dd
terryxiao07 <131728510+terryxiao07@users.noreply.github.com>
2023-10-12 14:23:25 -0400

    early attempt at tryting to implement rough notation
    not function breh

--- src/components/Annotations.jsx
+++ src/components/Annotations.jsx
@@ -1,5 +1,8 @@
-import { annotate } from "https://unpkg.com/rough-notation?module";
+import { annotate } from "rough-notation";
 
+const Annotations = () => {
+  
+}
 const a1 = annotate(document.querySelector("#title"), { type: "box", color: "yellow", paddingLeft: 5});
 const a2 = annotate(document.querySelector("#subtitle"), { type: "underline", color: "yellow" });
 
---
+++ src/testing.js
@@ -0,0 +1,3 @@
+import { annotate } from "rough-notation"
+
+annotate(document.querySelector("p"), { type: 'circle', color: "#0D47A1" }).show();
